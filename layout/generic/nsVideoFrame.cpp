284a1ba88db40f1ceb64dbb5ef446b8ef2e227f0
Dale Karp <me@dale.io>
2013-01-23 20:06:57 -0500

    hopefully things will build with references to webvtt out of the picture

--- layout/generic/nsVideoFrame.cpp
+++ layout/generic/nsVideoFrame.cpp
@@ -15,6 +15,7 @@
 #include "nsHTMLVideoElement.h"
 #include "nsIDOMHTMLVideoElement.h"
 #include "nsIDOMHTMLImageElement.h"
+#include "nsIDOMHTMLElement.h"
 #include "nsDisplayList.h"
 #include "gfxContext.h"
 #include "gfxImageSurface.h"
@@ -63,6 +64,8 @@ nsVideoFrame::CreateAnonymousContent(nsTArray<ContentInfo>& aElements)
 {
   nsNodeInfoManager *nodeInfoManager = GetContent()->GetCurrentDoc()->NodeInfoManager();
   nsCOMPtr<nsINodeInfo> nodeInfo;
+  Element *element;
+
   if (HasVideoElement()) {
     // Create an anonymous image element as a child to hold the poster
     // image. We may not have a poster image now, but one could be added
@@ -72,7 +75,7 @@ nsVideoFrame::CreateAnonymousContent(nsTArray<ContentInfo>& aElements)
                                             kNameSpaceID_XHTML,
                                             nsIDOMNode::ELEMENT_NODE);
     NS_ENSURE_TRUE(nodeInfo, NS_ERROR_OUT_OF_MEMORY);
-    Element* element = NS_NewHTMLImageElement(nodeInfo.forget());
+    element = NS_NewHTMLImageElement(nodeInfo.forget());
     mPosterImage = element;
     NS_ENSURE_TRUE(mPosterImage, NS_ERROR_OUT_OF_MEMORY);
 
@@ -112,6 +115,30 @@ nsVideoFrame::CreateAnonymousContent(nsTArray<ContentInfo>& aElements)
   if (!aElements.AppendElement(mVideoControls))
     return NS_ERROR_OUT_OF_MEMORY;
 
+  // Set up the caption overlay div for showing any TextTrack data
+  nodeInfo = nodeInfoManager->GetNodeInfo(nsGkAtoms::div,
+					  nullptr,
+					  kNameSpaceID_XHTML,
+					  nsIDOMNode::ELEMENT_NODE);
+  NS_ENSURE_TRUE(nodeInfo, NS_ERROR_OUT_OF_MEMORY);
+  element = NS_NewHTMLDivElement(nodeInfo.forget());
+  mCaptionDiv = element;
+  NS_ENSURE_TRUE(mCaptionDiv, NS_ERROR_OUT_OF_MEMORY);
+  // hang a class name on the div for default styling
+  nsresult rv = mCaptionDiv->SetAttr(kNameSpaceID_None,
+				     nsGkAtoms::_class,
+				     NS_LITERAL_STRING("captionDiv"),
+				     true);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (!aElements.AppendElement(mCaptionDiv))
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  // add a test string so we can test the div
+  nsCOMPtr<nsIDOMHTMLElement> div = do_QueryInterface(mCaptionDiv);
+  rv = div->SetInnerHTML(NS_LITERAL_STRING("<p>Lorem ipsum dolor</p>"));
+  NS_ENSURE_SUCCESS(rv, rv);
+
   return NS_OK;
 }
 
@@ -121,11 +148,13 @@ nsVideoFrame::AppendAnonymousContentTo(nsBaseContentList& aElements,
 {
   aElements.MaybeAppendElement(mPosterImage);
   aElements.MaybeAppendElement(mVideoControls);
+  aElements.MaybeAppendElement(mCaptionDiv);
 }
 
 void
 nsVideoFrame::DestroyFrom(nsIFrame* aDestructRoot)
 {
+  nsContentUtils::DestroyAnonymousContent(&mCaptionDiv);
   nsContentUtils::DestroyAnonymousContent(&mVideoControls);
   nsContentUtils::DestroyAnonymousContent(&mPosterImage);
   nsContainerFrame::DestroyFrom(aDestructRoot);
@@ -246,7 +275,7 @@ nsVideoFrame::Reflow(nsPresContext*           aPresContext,
   for (nsIFrame *child = mFrames.FirstChild();
        child;
        child = child->GetNextSibling()) {
-    if (child->GetType() == nsGkAtoms::imageFrame) {
+    if (child->GetContent() == mPosterImage) {
       // Reflow the poster frame.
       nsImageFrame* imageFrame = static_cast<nsImageFrame*>(child);
       nsHTMLReflowMetrics kidDesiredSize;
@@ -286,7 +315,7 @@ nsVideoFrame::Reflow(nsPresContext*           aPresContext,
                         posterTopLeft.x, posterTopLeft.y, 0, aStatus);
       FinishReflowChild(imageFrame, aPresContext, &kidReflowState, kidDesiredSize,
                         posterTopLeft.x, posterTopLeft.y, 0);
-    } else if (child->GetType() == nsGkAtoms::boxFrame) {
+    } else if (child->GetContent() == mVideoControls) {
       // Reflow the video controls frame.
       nsBoxLayoutState boxState(PresContext(), aReflowState.rendContext);
       nsBoxFrame::LayoutChildAt(boxState,
@@ -295,6 +324,24 @@ nsVideoFrame::Reflow(nsPresContext*           aPresContext,
                                        mBorderPadding.top,
                                        aReflowState.ComputedWidth(),
                                        aReflowState.ComputedHeight()));
+    } else if (child->GetContent() == mCaptionDiv) {
+      // Reflow to caption div
+      nsHTMLReflowMetrics kidDesiredSize;
+      nsSize availableSize = nsSize(aReflowState.availableWidth,
+				    aReflowState.availableHeight);
+      nsHTMLReflowState kidReflowState(aPresContext,
+				       aReflowState,
+				       child,
+				       availableSize,
+				       aMetrics.width,
+				       aMetrics.height);
+      kidReflowState.SetComputedWidth(aReflowState.ComputedWidth());
+      kidReflowState.SetComputedHeight(aReflowState.ComputedHeight());
+      ReflowChild(child, aPresContext, kidDesiredSize, kidReflowState,
+		  mBorderPadding.left, mBorderPadding.top, 0, aStatus);
+      FinishReflowChild(child, aPresContext,
+			&kidReflowState, kidDesiredSize,
+			mBorderPadding.left, mBorderPadding.top, 0);
     }
   }
   aMetrics.SetOverflowAreasToDesiredBounds();
@@ -386,17 +433,13 @@ nsVideoFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
-  // Add child frames to display list. We expect up to two children, an image
-  // frame for the poster, and the box frame for the video controls.
+  // Add child frames to display list. We expect various children,
+  // but only want to draw mPosterImage conditionally. Others we
+  // always add to the display list.
   for (nsIFrame *child = mFrames.FirstChild();
        child;
        child = child->GetNextSibling()) {
-    if (child->GetType() == nsGkAtoms::imageFrame && ShouldDisplayPoster()) {
-      rv = child->BuildDisplayListForStackingContext(aBuilder,
-                                                     aDirtyRect - child->GetOffsetTo(this),
-                                                     &replacedContent);
-      NS_ENSURE_SUCCESS(rv,rv);
-    } else if (child->GetType() == nsGkAtoms::boxFrame) {
+    if (child->GetContent() != mPosterImage || ShouldDisplayPoster()) {
       rv = child->BuildDisplayListForStackingContext(aBuilder,
                                                      aDirtyRect - child->GetOffsetTo(this),
                                                      &replacedContent);
