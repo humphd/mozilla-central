eea1b360f5ad361a81157948320507d026ab6444
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2013-02-15 14:07:04 -0500

    Revert opera test changes

--- dom/imptests/html.mk
+++ dom/imptests/html.mk
@@ -1,8 +1,7 @@
 DIRS += \
+  html/html/webappapis/scripting/events \
   html/html/browsers/browsing-the-web/read-media \
   html/html/semantics/scripting-1/the-script-element \
-	html/html/semantics/text-level-semantics/the-time-element \
-  html/html/webappapis/scripting/events \
   html/html/webappapis/scripting/processing-model-2 \
-  html/html/old-tests/submission/Opera/microdata \
+  html/old-tests/submission/Opera/microdata \
   $(NULL)
